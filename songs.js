5c08025f2e95cab15e6995e893649bb05ac4a379
LuisFernandoRufato <luis.rufato@gmail.com>
2023-10-19 15:48:00 -0300

    Projeto finalizado

---
+++ songs.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      src: "./src/Alice In Chains - Would.mp3",
+      name: "Alice In Chains - Would",
+    },
+    {
+      src: "./src/Audioslave - Be Yourself.mp3",
+      name: "Audioslave - Be Yourself",
+    },
+    {
+      src: "./src/Foo Fighters - Best Of You.mp3",
+      name: "Foo Fighters - Best Of You",
+    },
+    {
+      src: "./src/Pearl Jam - I Am Mine.mp3",
+      name: "Pearl Jam - I Am Mine",
+    },
+    {
+      src: "./src/Stone Temple Pilots - Plush.mp3",
+      name: "Stone Temple Pilots - Plush",
+    },
+    {
+      src: "./src/Three Days Grace - I Hate Everything About You.mp3",
+      name: "Three Days Grace - I Hate Everything About You",
+    },
+  ];
